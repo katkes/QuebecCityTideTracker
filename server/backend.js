46561ec49115f4a7159120091a948046920e31f6
katkes <keshank1@hotmail.com>
2023-12-24 21:29:22 -0500

    Rework of project and downloading node modules
    Node modules required:
    - Express
    - Axios
    - Cherrio

---
+++ server/backend.js
@@ -0,0 +1,34 @@
+// import axios
+// import cheerio
+const express = require('express')
+const app = express();
+const axios = require('axios')
+const cheerio = require('cheerio')
+const port = process.env.PORT || 8000;
+
+app.use(express.static('client')); // Serves main page from client directory
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
+async function getTide() {
+    const url1 = "https://www.tides.gc.ca/en/stations/03250/2023-08-04?tz=EDT&unit=m";
+    try {
+        const response = await axios.get(url1);
+        const $ = cheerio.load(response.data);
+
+        const pred = $("#readings-list-predictions tbody").text().split("\n");
+
+        // Readings are done in increments of 15 mins
+        console.log(pred[2].trim()); // current time
+        console.log(pred[3].trim()); // current reading (in m)
+        console.log(pred[6].trim()); // next 15 min
+        console.log(pred[7].trim()); // next 15 min reading (in m)
+        console.log(pred[10].trim());
+        console.log(pred[11].trim());
+    } catch (error) {
+        console.error(error);
+    }
+}
+getTide();
