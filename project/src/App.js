35b8087f905bae11da343c118a5699041893bcbb
katkes <keshank1@hotmail.com>
2024-01-02 23:42:42 -0500

    Refactoring of App.js file
    Deletion of old code

--- project/src/App.js
+++ project/src/App.js
@@ -1,48 +1,17 @@
 import React, {useEffect, useState} from "react";
-import axios from 'axios';
-import cheerio from 'cheerio';
+// import axios from 'axios';
+// import cheerio from 'cheerio';
 import {tideScrape} from './tideScrape';
 import WindyWidget1 from './windyWidget1';
-import WindyWidget2 from './windyWidget2';
+// import WindyWidget2 from './windyWidget2';
 
 function App() {
 
-    // const [info, setInfo] = useState([]);
     const [arr, setArr] = useState([]);
 
     const getInfo = async (e) => {
-
         const data = await tideScrape();
         setArr(data);
-
-        // try {
-        //     // Axios request to site via proxy server
-        //     const response = await axios.get('/api/en/stations/03250/2023-08-04?tz=EDT&unit=m');
-        //     const $ = cheerio.load(response.data);
-        //
-        //     const pred = $("#readings-list-predictions tbody").text().split("\n");
-        //
-        //     // Readings are done in increments of 15 mins
-        //     let temp1 = pred[2].trim(); // current time
-        //     let temp2 = pred[3].trim(); // current reading (in m)
-        //     let temp3 = pred[6].trim(); // next 15 min
-        //     let temp4 = pred[7].trim(); // next 15 min reading (in m)
-        //     let temp5 = pred[10].trim();
-        //     let temp6 = pred[11].trim();
-        //
-        //     console.log(pred[2].trim());
-        //     console.log(pred[3].trim());
-        //     console.log(pred[6].trim());
-        //     console.log(pred[7].trim());
-        //     console.log(pred[10].trim());
-        //     console.log(pred[11].trim());
-        //
-        //     let arr = [[temp1, temp2], [temp3, temp4], [temp5, temp6]];
-        //     setArr(arr);
-        //
-        // } catch (error) {
-        //     console.error(error);
-        // }
     }
 
     useEffect(() => {
