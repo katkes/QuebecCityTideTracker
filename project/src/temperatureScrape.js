1ddc4b898139034073b6c1b05846b935562d1e80
katkes <keshank1@hotmail.com>
2024-01-04 23:59:33 -0500

    Scrape for weather info
    Creation of temperatureScrape.js file

---
+++ project/src/temperatureScrape.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import cheerio from 'cheerio';
+
+export const weatherScrape = async () => {
+    try {
+        // Axios request to site via proxy server
+        const response = await axios.get('/api/en/stations/03250/2023-08-04?tz=EDT&unit=m');
+        const $ = cheerio.load(response.data);
+        const weather = $("#cellsTable > tbody > tr.windywidgetairTemp.id-air-temp > td:nth-child(1)").text().trim();
+        return weather;
+    } catch (error) {
+        console.error(error);
+    }
+}
